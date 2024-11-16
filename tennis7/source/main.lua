15f90316d37195eff1f94bac01e40f8d4975dd5c
Brett Chalupa <brettchalupa@gmail.com>
2024-11-16 13:29:10 -0500

    Remove pdx files

---
+++ tennis7/source/main.lua
@@ -0,0 +1,42 @@
+import "CoreLibs/graphics"
+
+local paddle = {
+	x = 20,
+	y = 20,
+	s = 10,
+	w = 12,
+	h = 48,
+}
+
+local ball = {
+	x = 60,
+	y = 80,
+	r = 10,
+}
+
+function playdate.update()
+	local displayHeight = playdate.display.getHeight()
+
+	if playdate.buttonIsPressed(playdate.kButtonUp) then
+		paddle.y -= paddle.s
+	end
+
+	if playdate.buttonIsPressed(playdate.kButtonDown) then
+		paddle.y += paddle.s
+	end
+
+	local _crankChange, acceleratedCrankChange = playdate.getCrankChange()
+	paddle.y += acceleratedCrankChange
+
+	if paddle.y <= 0 then
+		paddle.y = 0
+	end
+
+	if paddle.y + paddle.h >= displayHeight then
+		paddle.y = displayHeight - paddle.h
+	end
+
+	playdate.graphics.clear()
+	playdate.graphics.drawRect(paddle.x, paddle.y, paddle.w, paddle.h)
+	playdate.graphics.drawCircleAtPoint(ball.x, ball.y, ball.r)
+end
