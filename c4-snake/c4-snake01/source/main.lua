c2398a5e0307e5d08e6b8d09c093aedc95f8c553
Brett Chalupa <brettchalupa@gmail.com>
2024-11-18 21:16:03 -0500

    add first 3 snake sections

---
+++ c4-snake/c4-snake01/source/main.lua
@@ -0,0 +1,30 @@
+local gfx <const> = playdate.graphics
+local gridSize <const> = 20
+local screenWidth <const> = playdate.display.getWidth()
+local gridWidth <const> = screenWidth / gridSize
+
+local snake = {
+	gridX = 2,
+	gridY = 4,
+}
+
+local updates = 0
+
+function playdate.update()
+	updates += 1
+
+	updateSnake()
+	
+	gfx.clear()
+	gfx.fillRect(snake.gridX * gridSize, snake.gridY * gridSize, gridSize, gridSize)
+end
+
+function updateSnake()
+	if updates % 4 == 0 then
+		snake.gridX += 1
+	end
+
+	if snake.gridX > gridWidth then
+		snake.gridX = 0
+	end
+end
