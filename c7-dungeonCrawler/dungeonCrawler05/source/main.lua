915d3020230e3813078da380a274075f911e252a
Brett Chalupa <brett@brettchalupa.com>
2025-11-12 10:25:27 -0500

    Sync: Update c7-dungeonCrawler/dungeonCrawler02/dungeon-crawler.tiled-session, Update c7-dungeonCrawler/dungeonCrawler04/source/main.lua, Add c7-dungeonCrawler/dungeonCrawler05/dungeon-crawler.tiled-project and 8 more

--- c7-dungeonCrawler/dungeonCrawler05/source/main.lua
+++ c7-dungeonCrawler/dungeonCrawler05/source/main.lua
@@ -1,3 +1,5 @@
+import("CoreLibs/graphics")
+
 local gfx <const> = playdate.graphics
 
 local TILE_SIZE <const> = 8
@@ -17,18 +19,51 @@ local tileProp <const> = {
 local tileProps <const> = {
 	[18] = tileProp.WALKABLE, -- ground
 }
+local fontNontendo = gfx.font.new("fonts/Nontendo-Bold")
+gfx.setFont(fontNontendo)
+local synth = playdate.sound.synth.new(playdate.sound.kWaveSine)
+local focusedNPC = nil
+local levelName = nil
+local npcs <const> = {
+	level1 = {
+		["22,10"] = {
+			name = "Dog",
+			text = "Got a treat?",
+		},
+		["23,4"] = {
+			name = "Snake",
+			text = "Dogs, all they think about is food.",
+		},
+		["24,7"] = {
+			name = "Column",
+			text = "Sure is windy out here today.",
+		},
+	},
+}
 
 function playdate.update()
-	updatePlayer(player, map)
-	updateCamera(camera, player)
+	if focusedNPC then
+		if playdate.buttonJustPressed(playdate.kButtonA) then
+			playSFX("C5")
+			focusedNPC = nil
+		end
+	else
+		updatePlayer(player, map)
+		updateCamera(camera, player)
+	end
 
 	gfx.clear()
 
-	map:draw(-camera.x, -camera.y)
-	drawPlayer(player)
+	if focusedNPC then
+		drawTextbox(focusedNPC)
+	else
+		map:draw(-camera.x, -camera.y)
+		drawPlayer(player)
+	end
 end
 
-function loadLevel(levelName, imgtable)
+function loadLevel(name, imgtable)
+	levelName = name
 	local level = levels[levelName]
 	print("Loading level: " .. levelName)
 	local map = gfx.tilemap.new()
@@ -64,11 +99,19 @@ function updatePlayer(p, map)
 	local d = getInputDelta()
 
 	if d.x ~= 0 or d.y ~= 0 then
-		local tileAtDest = map:getTileAtPosition(p.x + d.x + 1, p.y + d.y + 1)
+		local dest = { x = p.x + d.x, y = p.y + d.y }
+
+		local tileAtDest = map:getTileAtPosition(dest.x + 1, dest.y + 1)
 
 		if tileProps[tileAtDest] == tileProp.WALKABLE then
 			p.x += d.x
 			p.y += d.y
+		else
+			focusedNPC = npcs[levelName][dest.x .. "," .. dest.y]
+
+			if focusedNPC then
+				playSFX("B3")
+			end
 		end
 	end
 end
@@ -98,6 +141,21 @@ end
 function updateCamera(c, p)
 	c.x = p.x * TILE_SIZE - (DISPLAY_WIDTH / 2)
 	c.y = p.y * TILE_SIZE - (DISPLAY_HEIGHT / 2)
+end
+
+function playSFX(note)
+	synth:playMIDINote(note, 1, 0.15)
+end
+
+function drawTextbox(speak)
+	gfx.setImageDrawMode(playdate.graphics.kDrawModeFillWhite)
+	gfx.drawText(speak.name, 4, 0)
+	gfx.setImageDrawMode(playdate.graphics.kDrawModeCopy)
+
+	playdate.graphics.setColor(gfx.kColorWhite)
+	gfx.fillRoundRect(0, DISPLAY_HEIGHT - 46, DISPLAY_WIDTH, 46, 4)
+	playdate.graphics.setColor(gfx.kColorBlack)
+	gfx.drawTextInRect(speak.text, 4, DISPLAY_HEIGHT - 42, DISPLAY_WIDTH - 8, 40)
 end
 
 init()
